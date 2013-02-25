69bc8518d7f60493e8722edaa3d58d54eddbe3af
Conrad Olega <conradolega@gmail.com>
2013-02-25 10:43:10 +0800

    Sending messages somehow works

--- src/Client.java
+++ src/Client.java
@@ -7,17 +7,38 @@ import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 
-public class Client extends BasicGameState {
-
-	String msg = "Initial message";
+class MsgThread extends Thread {
+	
 	Socket socket;
 	MyConnection conn;
+	String msg;
+	
+	public MsgThread(Socket socket) {
+		this.socket = socket;
+		this.conn = new MyConnection(socket);
+		this.msg = "connected!";
+	}
+	
+	public void run() {
+		while (true) {
+			msg = conn.getMessage();
+			System.out.println("Message: " + msg);
+		}
+	}
+}
+
+public class Client extends BasicGameState {
+
+	Socket socket;
+	MyConnection conn;
+	MsgThread thread;
+	String msg;
 	
 	public Client(int state) {
 		try {
 			socket = new Socket("127.0.0.1", 8888);
-			conn = new MyConnection(socket);
-			msg = "Connected!";
+			thread = new MsgThread(socket);
+			thread.start();
 		} catch (Exception e) {
 			
 		}
@@ -26,27 +47,23 @@ public class Client extends BasicGameState {
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg)
 			throws SlickException {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 			throws SlickException {
-		// TODO Auto-generated method stub
 		g.drawString(msg, 20, 20);
 	}
 
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta)
 			throws SlickException {
-		// TODO Auto-generated method stub
-		msg = conn.getMessage();
+		msg = thread.msg;
+		System.out.println("Message: " + msg);
 	}
 
 	@Override
 	public int getID() {
-		// TODO Auto-generated method stub
 		return 2;
 	}
 }
